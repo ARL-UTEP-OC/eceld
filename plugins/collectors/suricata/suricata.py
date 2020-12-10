ec918845b45a869f1eedf91fe260c68df04ef6eb
balde13 <baldeg6418@gmail.com>
2020-12-09 22:27:59 -0500

    suricata.py

---
+++ plugins/collectors/suricata/suricata.py
@@ -0,0 +1,36 @@
+import os
+import definitions
+from engine.collector import AutomaticCollector
+import shlex
+import os
+import subprocess
+from jinja2 import Environment, FileSystemLoader
+import sys, traceback
+import time
+import shutil
+
+class suricata(AutomaticCollector):
+    def suricata_config_cmd(self, command):
+        runcmd = shlex.split(command)
+
+        try:
+            subprocess.Popen(runcmd,
+                             shell=False,
+                             cwd=self.base_dir,
+                             stdout=self.devnull,
+                             stderr=self.devnull)
+
+        except OSError as err:
+            self.logger.error("Error attempting to run command in collector: %s | command: %s\n" % (self.name, command))
+            self.logger.error("System Error:" + str(err))
+
+    def build_commands(self):
+        # build commands
+        cmd = "sudo service suricata start"
+        self.commands.append(cmd)
+
+    def terminate(self):
+        #additional logic to stop suricata using the service subsystem
+        self.suricata_config_cmd("sudo service suricata stop")
+        #Now stop the collector
+        super(suricata, self).terminate()
